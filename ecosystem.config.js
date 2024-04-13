5b0a8028660c66bd12121ce7289da10c6d6cc2dd
MatheusFuzari <matheus.fuzari004@gmail.com>
2024-04-12 22:51:55 -0300

    ecosysNew

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    apps: [
-      {
-        name: 'NuxtAppName',
-        port: '3000',
-        exec_mode: 'cluster',
-        instances: 'max',
-        script: './.output/server/index.mjs'
-      }
-    ]
-  }
+  apps: [
+    {
+      name: 'NuxtAppName', //replace with your project name
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start'
+    }
+  ]
+}
